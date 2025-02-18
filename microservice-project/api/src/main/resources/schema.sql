84b3b9f6144d03e5e0c4ae5ad139470af6febaef
vishrut.bhushan <vbhushan75@gmail.com>
2025-02-19 00:14:37 +0530

    Change

--- microservice-project/api/src/main/resources/schema.sql
+++ microservice-project/api/src/main/resources/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE "GraphVisualizer".Graphs (
 );
 
 -- Insert a new graph into the Graphs table
-INSERT IGNORE INTO "GraphVisualizer".Graphs (name, description, created_user, adjacency_matrix, all_nodes)
+INSERT INTO "GraphVisualizer".Graphs (name, description, created_user, adjacency_matrix, all_nodes)
 VALUES ('Graph A', 'Description for Graph A', 'UserA', 
     jsonb_build_array(
         jsonb_build_array(jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 2, 'highlighted', false), jsonb_build_object('weight', 1, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false)), 
@@ -22,10 +22,11 @@ VALUES ('Graph A', 'Description for Graph A', 'UserA',
         jsonb_build_array(jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false))
     ), 
     jsonb_build_object('a', jsonb_build_object('x', 100, 'y', 100, 'index', 0, 'highlighted', false), 'b', jsonb_build_object('x', 171, 'y', 399, 'index', 1, 'highlighted', false), 'c', jsonb_build_object('x', 606, 'y', 265, 'index', 2, 'highlighted', false))
-);
+)
+ON CONFLICT DO NOTHING;
 
 -- Insert another graph into the Graphs table
-INSERT IGNORE INTO "GraphVisualizer".Graphs (name, description, created_user, adjacency_matrix, all_nodes)
+INSERT INTO "GraphVisualizer".Graphs (name, description, created_user, adjacency_matrix, all_nodes)
 VALUES ('Graph B', 'Description for Graph B', 'UserB', 
     jsonb_build_array(
         jsonb_build_array(jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 1, 'highlighted', false), jsonb_build_object('weight', 2, 'highlighted', false), jsonb_build_object('weight', -1, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false)), 
@@ -34,4 +35,5 @@ VALUES ('Graph B', 'Description for Graph B', 'UserB',
         jsonb_build_array(jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 2, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false), jsonb_build_object('weight', 0, 'highlighted', false))
     ), 
     jsonb_build_object('a', jsonb_build_object('x', 99, 'y', 99, 'index', 0, 'highlighted', false), 'b', jsonb_build_object('x', 561, 'y', 370, 'index', 1, 'highlighted', false), 'c', jsonb_build_object('x', 521, 'y', 129, 'index', 2, 'highlighted', false), 'd', jsonb_build_object('x', 192, 'y', 293, 'index', 3, 'highlighted', false))
-);
+)
+ON CONFLICT DO NOTHING;
